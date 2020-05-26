6ca9778083eab918b8cb904f6d269cd931b3d6ac
Matheus-Fernandes21 <63601334+Matheus-Fernandes21@users.noreply.github.com>
2020-05-26 13:21:00 -0300

    Add files via upload

---
+++ while2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class while2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int num = 0;
+	     int valor;
+		
+		Scanner leitor = new Scanner(System.in);
+       
+        System.out.print("Digite um número:");
+        valor = leitor.nextInt();
+        while(num <= valor){
+            System.out.println(num);
+            num++;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
+}
